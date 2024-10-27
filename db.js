e4ae1a51863c89b16d2da0bfeb60e7377b25b932
SujalDudhe <sujaldudhe2004@gmail.com>
2024-10-27 17:43:22 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "272004@SMD",
+  user: "your_username",
+  password: "your_password",
   database: "todo_db",
 });
 
